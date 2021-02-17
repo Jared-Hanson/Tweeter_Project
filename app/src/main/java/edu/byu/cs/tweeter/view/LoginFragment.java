7daac0f15e812d5ed4a4e7a2c37d569316ffcd11
levifleming <lrfleming96@gmail.com>
2021-02-16 17:51:59 -0700

    loginfragment

--- app/src/main/java/edu/byu/cs/tweeter/view/LoginFragment.java
+++ app/src/main/java/edu/byu/cs/tweeter/view/LoginFragment.java
@@ -1,19 +1,28 @@
 package edu.byu.cs.tweeter.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import edu.byu.cs.tweeter.R;
-import edu.byu.cs.tweeter.presenter.FollowerPresenter;
+import edu.byu.cs.tweeter.model.service.request.LoginRequest;
+import edu.byu.cs.tweeter.model.service.response.LoginResponse;
+import edu.byu.cs.tweeter.presenter.LoginPresenter;
+import edu.byu.cs.tweeter.view.asyncTasks.LoginTask;
+import edu.byu.cs.tweeter.view.main.MainActivity;
 
-public class LoginFragment extends Fragment implements FollowerPresenter.View {
+public class LoginFragment extends Fragment implements LoginPresenter.View, LoginTask.Observer {
     private static final String LOG_TAG = "LoginFragment";
 
-    private FollowerPresenter presenter;
+    private LoginPresenter presenter;
+    private Toast loginInToast;
 
     /**
      * Creates an instance of the fragment and places the user and auth token in an arguments
@@ -31,8 +40,60 @@ public class LoginFragment extends Fragment implements FollowerPresenter.View {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
-        presenter = new FollowerPresenter(this);
+        presenter = new LoginPresenter(this);
+
+        Button loginButton = view.findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginInToast = Toast.makeText(getActivity(), "logging in", Toast.LENGTH_LONG);
+                loginInToast.show();
+
+                LoginRequest loginRequest = new LoginRequest("dummyUserName", "dummyPassword");
+                LoginTask loginTask = new LoginTask(presenter, LoginFragment.this);
+                loginTask.execute(loginRequest);
+            }
+        });
 
         return view;
+    }
+
+    /**
+     * The callback method that gets invoked for a successful login. Displays the MainActivity.
+     *
+     * @param loginResponse the response from the login request.
+     */
+    @Override
+    public void loginSuccessful(LoginResponse loginResponse) {
+        Intent intent = new Intent(getContext(), MainActivity.class);
+
+        intent.putExtra(MainActivity.CURRENT_USER_KEY, loginResponse.getUser());
+        intent.putExtra(MainActivity.AUTH_TOKEN_KEY, loginResponse.getAuthToken());
+
+        loginInToast.cancel();
+        startActivity(intent);
+    }
+
+    /**
+     * The callback method that gets invoked for an unsuccessful login. Displays a toast with a
+     * message indicating why the login failed.
+     *
+     * @param loginResponse the response from the login request.
+     */
+    @Override
+    public void loginUnsuccessful(LoginResponse loginResponse) {
+        Toast.makeText(getActivity(), "Failed to login. " + loginResponse.getMessage(), Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * A callback indicating that an exception was thrown in an asynchronous method called on the
+     * presenter.
+     *
+     * @param exception the exception.
+     */
+    @Override
+    public void handleException(Exception exception) {
+        Log.e(LOG_TAG, exception.getMessage(), exception);
+        Toast.makeText(getActivity(), "Failed to login because of exception: " + exception.getMessage(), Toast.LENGTH_LONG).show();
     }
 }
