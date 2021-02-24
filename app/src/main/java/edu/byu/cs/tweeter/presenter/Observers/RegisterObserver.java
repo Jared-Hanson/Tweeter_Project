b886581b423ce2db4e2762fb3b1d77377dd599a6
levifleming <lrfleming96@gmail.com>
2021-02-24 09:30:47 -0700

    register more robust

--- app/src/main/java/edu/byu/cs/tweeter/presenter/Observers/RegisterObserver.java
+++ app/src/main/java/edu/byu/cs/tweeter/presenter/Observers/RegisterObserver.java
@@ -3,5 +3,5 @@ package edu.byu.cs.tweeter.presenter.Observers;
 import edu.byu.cs.tweeter.view.main.Login.RegisterSubject;
 
 public interface RegisterObserver {
-    void Update(RegisterSubject subject, String first, String last, String username, String password);
+    void Update(RegisterSubject subject, String first, String last, String username, String password, boolean hasPicture);
 }
